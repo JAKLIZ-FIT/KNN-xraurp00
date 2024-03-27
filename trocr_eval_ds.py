3a2db8900c01009a04561ac7718bb4caf485bb63
JAKLIZ-FIT <george.vein.z@gmail.com>
2024-03-27 05:40:43 +0100

    solving keys

--- trocr_eval_ds.py
+++ trocr_eval_ds.py
@@ -11,6 +11,8 @@ from trocr.src.scripts import predict
 from dataset import LMDBDataset
 from context import Context
 
+import pandas as pd
+
 # TODO
 # load val/test ds
 # load image db
@@ -30,7 +32,7 @@ def predict_for_ds(labels_path: Path, ds_path: Path, use_local_model: bool = Tru
         label_file=labels_path,
         processor=processor
     )
-    dataloader = DataLoader(ds, 20, shuffle=True, num_workers=8)
+    dataloader = DataLoader(ds, 1, shuffle=True, num_workers=8) # batchsize was originally 20
     model = load_model(use_local_model)
 
     """
@@ -70,6 +72,19 @@ def parse_args():
     )
     return parser.parse_args()
 
+def compute_metrics(pred):
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions
+
+    processor = load_processor()
+    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+    labels_ids[labels_ids == -100] = processor.tokenizer.pad_token_id
+    label_str = processor.batch_decode(labels_ids, skip_special_tokens=True)
+
+    cer = cer_metric.compute(predictions=pred_str, references=label_str)
+
+    return {"cer": cer}
+
 def main():
     args = parse_args()
 
@@ -80,11 +95,30 @@ def main():
     )
 
     # TODO - remove
+    print(output[:10])
+    print(confidence[:10])
+    
     for o, c in zip(output, confidence):
         print(f'File: {o[0]}, prediction: {o[1]}, confidence: {c[1]}')
-
+    
+    df_output = pd.DataFrame(output, columns =['idx', 'output'])
+    df_output['sep1'] = 0
+    df_confid = pd.DataFrame(confidence, columns =['idx', 'confidence'])
+    df_confid['sep2'] = 0
+    df_combi = pd.merge(df_output, df_confid, on='idx', sort=True)
+    df_combi = df_combi['idx','sep1','output','sep2','confidence']
+    df_combi.to_csv("out.csv",sep=" ")
+    
+    
     # TODO
+    from datasets import load_metric
+
+    cer_metric = load_metric("cer")
+    
+    
     # calculate CER, WER
+    
+    
     # put data to file
 
     return 0
