8eceb345a910d03b5e7f724ec46628a73429ea54
Pavel Raur <pavel.raur@gmail.com>
2024-04-15 13:49:38 +0200

    Added messages for stages

--- train.sh
+++ train.sh
@@ -13,24 +13,29 @@ export SRC_MODEL='trocr-base-stage1'
 export OUT_MODEL='base-stage1-supervised'
 
 # add env module
+echo "Adding py-virtualenv module."
 module add py-virtualenv
 # change tmp dir
 export TMPDIR=$SCRATCHDIR
 # create env
+echo "Creating virtual environment."
 python3 -m venv $SCRATCHDIR/venv
 . $SCRATCHDIR/venv
 # install requirements
+echo "Installing dependencies."
 pip install -U pip
 pip install -r $PROJ_SRC/requirements.txt
 # clean pip cache
 pip cache purge
 # copy ds
+echo "Creating copy of required files in the scratch dir."
 mkdir $SCRATCHDIR/ds
 cp -r $DATA/bentham_self-supervised $SCRATCHDIR/ds
 # copy sw
 mkdir $SCRATCHDIR/src
 cp -r $PROJ_SRC $SCRATCHDIR/src
 # run training
+echo "Running the trocr_train.py script."
 cd $SCRATCHDIR/src
 python trocr_train.py \
     -m $MODELS/$SRC_MODEL \
@@ -42,8 +47,10 @@ python trocr_train.py \
     -s $MODELS/$OUT_MODEL
 # TODO - add arg for gpu
 # clean scratch
+echo "Cleaning up."
 cd ~
 deactivate
 rm -rf $SCRATCHDIR/*
 # unload module
 module purge
+echo "Batch finished."
