f666659ff695011fe52cfec3b62aa8bcbe317975
Pavel Raur <pavel.raur@gmail.com>
2024-05-10 05:29:12 +0200

    Added split to buckets and metric eval for buckets

--- eval_metrics.py
+++ eval_metrics.py
@@ -23,7 +23,10 @@ class MetricsEvaluator:
             '_resize.jpg',
             '_skewed.jpg',
             '_mask.jpg',
-            '_noise.jpg'
+            '_noise.jpg',
+            '_rotate.jpg',
+            '_mixup.jpg',
+            '_cutmix.jpg'
         ]
         self.dataset = self.load_stat_file(file=file)
     
@@ -113,7 +116,8 @@ class MetricsEvaluator:
             )
             predictions = [data['predictions']]
             for f in augmented_data_files:
-                predictions.append(self.dataset[f]['predictions'])
+                if f in self.dataset:
+                    predictions.append(self.dataset[f]['predictions'])
             
             data['conf_cn'] = self.gen_confusion_net_confidence(
                 predictions=predictions
@@ -159,6 +163,184 @@ class MetricsEvaluator:
         auc = np.trapz(cer_vals)
 
         return [x[0] for x in conf_cer], cer_vals, auc
+    
+    def get_buckets(
+        self,
+        n_buckets: int = 10
+    ) -> (list[list[str]], int):
+        """
+        Creates buckets based on lenght of the predicted labels.
+        :param n_buckets (int): number of buckets to create
+        :returns (list[list[str]], int): (
+            list of buckets of data ids/keys,
+            size of largest bucket
+        )
+        """
+        # get filenames of non-augmented data
+        filenames = []
+        for file_name, data in self.dataset.items():
+            if data['augmented']:
+                continue
+            filenames.append(file_name)
+        
+        # sort by transcription length
+        filenames.sort(key=lambda x: len(self.dataset[x]['predictions']))
+
+        # create buckets
+        buckets = []
+        step = int(len(filenames) / n_buckets)
+        partition_start = 0
+        partition_end = step
+        for i in range(n_buckets - 1):
+            bucket = filenames[partition_start:partition_end]
+            buckets.append(bucket)
+            partition_start = partition_end
+            partition_end += step
+        # align last partition
+        partition_end = len(filenames)
+        buckets.append(filenames[partition_start:partition_end])
+
+        # get longest bucket
+        max_size = max(map(len, buckets))
+
+        return buckets, max_size
+    
+    def sort_bucket_by_field(
+        self,
+        buckets: list[list[str]],
+        field_name: str
+    ) -> None:
+        """
+        Sorts buckets by given field.
+        :param buckets (list[list[str]]): list of buckets of data ids/keys
+        :param field_name (str): name of field to sort buckets by
+        :returns: None (buckets are modified in-place)
+        """
+        for bucket in buckets:
+            bucket.sort(
+                key=lambda x: self.dataset[x][field_name],
+                reverse=True
+            )
+
+    def eval_metric_buckets(
+        self,
+        confidence_type: str,
+        n_buckets: int = 10
+    ) -> (list[float], float):
+        """
+        Evaluates how well given confidence type corespondes to the CER.
+        Evaluation is done for data separated to buckets based on transcription
+        length. Data are taken from bucket according to their confidence.
+        CER corespondence is computed as
+            sum(CER of results) / number of results.
+        -> Smaller area under curve means better result! <-
+        :param confidence_type (str): name of confidence field to evaluate
+        :param n_buckets (int): number of buckets to separate dataset to
+        :returns (list[float], float): tuple(
+            CER values,
+            area under the curve for given metric
+        )
+        """
+        # get buckets and largest bucket size
+        buckets, n_steps = self.get_buckets(n_buckets=n_buckets)
+
+        # sort each bucket by confidence
+        self.sort_bucket_by_field(buckets=buckets, field_name=confidence_type)
+
+        cer_vals = []
+        # calculate metric for data taken from buckets
+        for i in range(n_steps):
+            tmp = []
+            for bucket in buckets:
+                tmp.extend(bucket[:i + 1])
+            tmp_cer = [self.dataset[fn]['cer'] for fn in tmp]
+            cer_vals.append(np.sum(tmp_cer) / ((i + 1) * n_buckets))
+        
+        # calculate area under curve
+        auc = np.trapz(cer_vals)
+
+        return cer_vals, auc
+
+    def bucket_select(
+        self,
+        confidence_type: str,
+        n_buckets: int = 10,
+        percentages: list[int] = [100],
+        output_file_prefix: str = None
+    ) -> None:
+        """
+        Selects data from buckets to be used in training.
+        :param confidence_type (str): confidence type to use for selection
+        :param n_buckets (int): number of buckets to use
+        :param percentages (list[int]): percentages of data to select
+        :param output_file_prefix (str): output file prefix
+        :returns: None (generates output to files)
+        """
+        # get buckets and largest bucket size
+        buckets, bucket_size = self.get_buckets(n_buckets=n_buckets)
+
+        # sort each bucket by confidence
+        self.sort_bucket_by_field(buckets=buckets, field_name=confidence_type)
+
+        # set prefix for output file
+        if not output_file_prefix:
+            output_file_prefix = confidence_type + '_bucket_'
+        
+        # generate output files
+        for percentage in percentages:
+            output_file = open(output_file_prefix + str(percentage), 'w')
+
+            # get output data
+            output = []
+            for bucket in buckets:
+                output.extend(bucket[:int(bucket_size*percentage/100)])
+            
+            # get augmented versions of augmented data
+            for fn in output:
+                aug = self.get_augmented_file_versions(fn)
+                for a in aug:
+                    if a in self.dataset:
+                        output.append(a)
+
+            # write output to file
+            for key in output:
+                output_file.write(
+                    f'{key} 0 {self.dataset[key]["predictions"]}\n'
+                )
+
+            output_file.close()
+
+    def save_bucket_metrics_to_file(
+        self,
+        file: Path,
+        confidence_types: list[str],
+        n_buckets: int
+    ):
+        """
+        Saves metrics to json file after evaluation.
+        :param file (pathlib.Path): path to file
+        :param confidence_types (list): list of confidence types to evaluate
+        :param n_buckets (int): number of buckets to use
+        :return (dict): results (same as saved to file)
+        """
+        results = {}
+        file = open(file, 'w')
+
+        for ct in confidence_types:
+            vals, area = self.eval_metric_buckets(
+                confidence_type=ct,
+                n_buckets=n_buckets
+            )
+            results[ct] = {
+                'conf': [],
+                'vals': vals,
+                'auc': area
+            }
+        
+        json.dump(results, file, indent=4)
+        file.close()
+
+        return results
 
     def save_metrics_to_file(
         self,
@@ -206,15 +388,58 @@ def parse_args():
         nargs='+',
         default=[]
     )
+    parser.add_argument(
+        '-b', '--number-of-buckets',
+        help='Number of buckets to split data to.',
+        type=int,
+        default=0
+    )
+    parser.add_argument(
+        '-s', '--selected-metric',
+        help='Metric selected to use for spliting data based on otput '
+             'percentage.',
+        default=None
+    )
+    parser.add_argument(
+        '-x', '--output-file-prefix',
+        help='Prefix of output files when saving buckets.',
+        default=None
+    )
+    parser.add_argument(
+        '-p', '--bucket-percentages',
+        help='Percentage (as whole numbers) of data selected data in output '
+             'file. Can be multiple values, to create multiple files.',
+        nargs='+',
+        type=int,
+        default=[100]
+    )
     return parser.parse_args()
 
 def main():
     args = parse_args()
     me = MetricsEvaluator(args.dataset)
     me.compute_cn_conf()
-    metrics = me.save_metrics_to_file(args.output_file, args.metrics_to_evaluate)
-    for metric, values in metrics.items():
-        print(f'Metric name: {metric}, metric auc: {values["auc"]}.')
+    if not args.selected_metric:
+        if not args.number_of_buckets:
+            metrics = me.save_metrics_to_file(
+                args.output_file,
+                args.metrics_to_evaluate
+            )
+        else:
+            metrics = me.save_bucket_metrics_to_file(
+                args.output_file,
+                args.metrics_to_evaluate,
+                n_buckets=args.number_of_buckets
+            )
+        for metric, values in metrics.items():
+            print(f'Metric name: {metric}, metric auc: {values["auc"]}.')
+    else:
+        me.bucket_select(
+            confidence_type=args.selected_metric,
+            n_buckets=args.number_of_buckets,
+            percentages=args.bucket_percentages,
+            output_file_prefix=args.output_file_prefix
+        )
     return 0
 
 
--- merge_dbs.py
+++ merge_dbs.py
@@ -39,8 +39,56 @@ def merge_dbs(src_db_paths: list[Path], target_db_path: Path) -> None:
         source_db.close()
     target_db.close()
 
+def separate_dbs(
+    source_path: Path,
+    target_path: Path,
+    ids_path: Path,
+    split_labels: bool = True
+) -> None:
+    """
+    Extracts part of the database to separate db.
+    :param source_path (pathlib.Path): source database path
+    :param target_path (pathlib.Path): target database path
+    :param ids_path (pathlib.Path): path to ids of data to extract
+    :param split_labels (bool): if lines in id files contains format
+        (id 0 label), this option splits the line and uses only id
+    """
+    if not source_path.exists():
+        raise ValueError(f"File {str(source_path)} does not exist!")
+    if not ids_path.exists():
+        raise ValueError(f"File {str(ids_path)} does not exist!")
+    
+    id_file = open(str(ids_path), 'r')
+    target_db = lmdb.open(str(target_path), create=True)
+    source_db = lmdb.open(str(source_path))
+    source_tx = source_db.begin()
+    
+    for i in id_file:
+        i = i.strip()
+        if split_labels:
+            i = i.split(' 0 ')[0]
+        i = i.encode()
+        target_tx = target_db.begin(write=True)
+        data = source_tx.get(i)
+        try:
+            target_tx.put(key=i, value=data)
+            target_tx.commit()
+        except lmdb.MapFullError:
+            target_tx.abort()
+            target_db.set_mapsize(target_db.info()['map_size'] * 2)
+            target_tx = target_db.begin(write=True)
+            target_tx.put(key=i, value=data)
+            target_tx.commit()
+
+    source_db.close()
+    target_db.close()
+    id_file.close()
+
 def parse_args():
-    parser = argparse.ArgumentParser('Merges lmdb databases together.')
+    parser = argparse.ArgumentParser(
+        'Merges lmdb databases together, '
+        'or extracts part of db to separate one.'
+    )
     parser.add_argument(
         '-s', '--source-databases',
         help='Source databases to merge.',
@@ -54,14 +102,35 @@ def parse_args():
         type=Path,
         required=True
     )
+    parser.add_argument(
+        '-i', '--ids-file',
+        help='File with ids if database separation should be used. '
+             'Only one source db must be used with this option.',
+        type=Path,
+        default=None
+    )
+    parser.add_argument(
+        '-l', '--label-file',
+        help='Tels if ids-file contains labels or not. (default=False)',
+        default=False,
+        action='store_true'
+    )
     return parser.parse_args()
 
 def main():
     args = parse_args()
-    merge_dbs(
-        src_db_paths=args.source_databases,
-        target_db_path=args.target_database
-    )
+    if not args.ids_file:
+        merge_dbs(
+            src_db_paths=args.source_databases,
+            target_db_path=args.target_database
+        )
+    else:
+        separate_dbs(
+            source_path=args.source_databases[0],
+            target_path=args.target_database,
+            ids_path=args.ids_file,
+            split_labels=args.label_file
+        )
     return 0
 
 
